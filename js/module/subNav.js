02b93242b369c4ba5f0c19f91acba388ad677127
wenhuikang1 <kang406864868@126.com>
2015-09-17 18:45:20 +0800

    v.26

--- js/module/subNav.js
+++ js/module/subNav.js
@@ -8,28 +8,30 @@ define(function() {
         $warper = $('.warper'),
         dfteq = $nav.find('.cur').index();
 
-    console.log(dfteq);
-
     function showSubNav() {
         if ($(this).index() == 1) {
             $subNavbg.toggleClass('anim');
             $warper.toggleClass('anim');
-            !$(this).hasClass('cur') && $(this).toggleClass('cur');
+            if(!$(this).hasClass('cur')){
+                $(this).addClass('cur');
+            }else{
+                $(this).removeClass('cur');
+            }
+            
         }
+        return false;
     }
 
     function hideSubNav() {
         if ($subNavbg.hasClass('anim')) {
             $subNavbg.removeClass('anim');
             $warper.removeClass('anim');
-            console.log(dfteq !=1);
-            if(dfteq !=1){
+            if (dfteq != 1 && $nav.find('li').eq(1).hasClass('cur')) {
                 $nav.find('li').eq(1).removeClass('cur');
             }
-            
+
         }
     }
-
 
     $nav.on('click', 'li', showSubNav);
     $nav.on('mouseenter', 'li:not(:eq(1))', hideSubNav);
